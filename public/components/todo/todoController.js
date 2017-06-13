990a4f231d7c66f9435832273363997bf71e9a55
rraila <reginaldas.raila@inntec.lt>
2017-06-13 20:24:12 +0300

    fixed todo service

--- public/components/todo/todoController.js
+++ public/components/todo/todoController.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('todo')
     .controller('TodoController', function ($scope, $http, TodoService) {
-			todoService.getTodoList().then(function(response) {
+			TodoService.getTodoList().then(function(response) {
 				$scope.todos = response.data;
 			},
 			function(err) {
